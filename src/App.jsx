b6cd144a4d083eacb46968941533b7c2cc2360bc
Wahab Khan <awkmadrista@gmail.com>
2026-02-03 20:06:07 +0500

    hero half

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../sections/Hero'
 
 function App() {
   return (
     <main>
       <Navbar/>
+      <Hero/>
     </main>  )
 }
 
