c39657cbeacdb85b76324756f150dd0473f9be4a
Wahab Khan <awkmadrista@gmail.com>
2026-02-03 19:53:33 +0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <main>
+      <Navbar/>
+    </main>  )
 }
 
 export default App
